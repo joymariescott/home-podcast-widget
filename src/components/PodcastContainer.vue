fbde80cf1f1a7432d6624e642450e98b4c990479
Kia Farhang <kfarhang0@gmail.com>
2017-12-22 14:23:08 -0600

    Add basic PodcastContainer component; will later hold async code

---
+++ src/components/PodcastContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <podcast
+    image="http://ww1.hdnux.com/photos/70/05/35/14702404/4/500x500.jpg"
+    chatter="Test chatter"
+    file="http://www.expressnews.com/file/261/7/2617-END122217.mp3"
+  />
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Podcast from "./Podcast.vue";
+
+export default Vue.extend({
+  components: {
+    Podcast
+  }
+});
+</script>
