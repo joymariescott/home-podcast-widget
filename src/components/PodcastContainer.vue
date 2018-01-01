57fa07fd8f2fb4c333fabb94965701e28a0f8aa5
Kia Farhang <kfarhang0@gmail.com>
2018-01-01 12:05:10 -0600

    PodcastContainer loads podcast feeds on mount, renders podcast components

--- src/components/PodcastContainer.vue
+++ src/components/PodcastContainer.vue
@@ -1,17 +1,47 @@
 <template>
+<div>
   <podcast
-    image="http://ww1.hdnux.com/photos/70/05/35/14702404/4/500x500.jpg"
-    chatter="Test chatter"
-    file="http://www.expressnews.com/file/261/7/2617-END122217.mp3"
+    v-for="(podcast, index) in podcasts"
+    :image="podcast.image"
+    :chatter="podcast.chatter"
+    :file="podcast.file"
+    :key="index"
   />
+  </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import Podcast from "./Podcast.vue";
+import {
+  Podcast as IPodcast,
+  PodcastMeta,
+  PodcastEpisode,
+  getPodcast
+} from "libsyn-feed-parser";
+import { podcastFeedUrls } from "../constants";
 
 export default Vue.extend({
   components: {
     Podcast
+  },
+  data() {
+    return {
+      podcasts: []
+    };
+  },
+  mounted() {
+    podcastFeedUrls.forEach(async (url: string) => {
+      try {
+        const podcast: IPodcast = await getPodcast(url);
+        const { meta, episodes } = podcast;
+
+        this.podcasts.push({
+          image: meta.imageURL,
+          chatter: meta.description,
+          file: episodes[0].audioFileURL
+        });
+      } catch (e) {}
+    });
   }
 });
 </script>
