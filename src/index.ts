f7ef283b955e76196a939459a941644eb05d4f35
Kia Farhang <kfarhang0@gmail.com>
2018-01-02 11:47:16 -0600

    Add code to destroy duplicate HTML added by Hearst CMS to index file; runs before instantiating Vue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import Vue from "vue";
 import PodcastContainer from './components/PodcastContainer.vue';
 
-const v = new Vue({
+// The EN CMS duplicates the HTML 'zone' where we place the root for
+// the podcast widget. This makes targetting it a pain. So we 
+// destroy whichever div is not visible based on screen size.
+
+const destroyDuplicateZone5 = (): void => {
+    const mobileZone = document.querySelector('div.zone-5a.narrow-only');
+    const wideZone = document.querySelector('div.zone-5.wide-only');
+
+    if (window.innerWidth < 768) {
+        deleteElement(wideZone);
+    } else {
+        deleteElement(mobileZone);
+    }
+    function deleteElement(element: Element) {
+        element.parentElement.removeChild(element);
+    }
+}
+
+try {
+    destroyDuplicateZone5();
+} catch (e) {
+
+}
+
+const homePodcastWidget = new Vue({
     el: "#home-podcast-root",
     template: `
         <podcast-container/>
